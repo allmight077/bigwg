9f70e76c88a1e8118d4565a31b4014693e4d6b6d
allmight077 <3191084206@qq.com>
2020-09-25 19:23:09 +0800

    init

---
+++ main.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a,b,c,d,e;
+    scanf("%d",&e);
+    if()
+
+    scanf("%d%d",&c,&d);
+    JOIA \
+    j bie
+    print()
+    {
+      printf(scanf )
+      if( )
+
+    }
+    scanf("%d%d",&a,&b);
+    if(e==1||e==3)
+    {
+        if(c>d)
+    {
+        a=a+b;
+        printf("%d",a);
+
+        printf(");
+    }
+    print
+    print
+    if(c==d)
+    printf("");kk
+    if(c<d)
+    {
+        a=a-b;
+        printf("%d",a);
+        ja
+        printf("");
+
+    }
+    }
+    if(e==2)
+    {
+        if(c>d)
+    {
+        a=a+2*b;
+        printf("%d",a);
+        printf("");
+    }
+    if(c==d)
+    printf("");
+    if(c<d)
+    {
+    eir
+        a=a-2*b;
+        printf("%d",a);
+        printf("");
+    }
+    }
+    if(e==4)
+    {
+        printf9("54  4")
+
+    }pr
+    char *str[3]={
+ "Hello,thisisasample!",
+ "Hi,goodmorning.",hhpringt
+ "Helloworld"
+  akjd
+};
+char s[80]；
+print (%j  ma  *    )
+strcpy(s,str[0]); //也可写成strcpy(s,*str);
+strcpy(s,str[1]); //也可写成strcpy(s,*(str+1));
+strcpy(s,str[2]); //也可写成strcpy(s,jia*(str+2));
+    if( kkkea )
+    return 0;
+}
